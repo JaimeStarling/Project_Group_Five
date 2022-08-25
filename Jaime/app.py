03beab73c060aa60b9f8868669ca13cce3d9f52e
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-08-25 18:38:55 -0500

    Add files via upload

---
+++ Jaime/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, redirect, request, jsonify
+
+# Crflaskeate an instance of Flask
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    # Return template and data
+    return render_template("index.html")
+
+@app.route("/about_us")
+def about_us():
+    # Return template and data
+    return render_template("about_us.html")
+
+@app.route("/indexwithtitanic")
+def indexwithtitanic():
+    # Return template and data
+    return render_template("indexwithtitanic.html")
+
+@app.route("/tableau")
+def tableau():
+    # Return template and data
+    return render_template("tableau.html")
+
+@app.route("/tableau2")
+def tableau2():
+    # Return template and data
+    return render_template("tableau2.html")
+
+@app.route("/tableau3")
+def tableau3():
+    # Return template and data
+    return render_template("tableau3.html")
+
+@app.route("/makePredictions", methods=["POST"])
+def make_predictions():
+    content = request.json["data"]
+    print(content)
+    return(jsonify({"ok": True}))
+
+#############################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+if __name__ == "__main__":
+    app.run(debug=True)
